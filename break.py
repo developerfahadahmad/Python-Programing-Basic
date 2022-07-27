7af1b34070b38f5457d7699427992e09712eb917
developerfahadahmad <mdshahariarhafiz2022@gmail.com>
2022-07-27 20:48:51 +0600

    Basic Concept

---
+++ break.py
@@ -0,0 +1,8 @@
+for i in range(0 , 50 ):
+    if i == 30: break
+    print(i)
+    
+
+for x in range(0 , 100 ):
+    if x == 70 : continue
+    print(x)
---
+++ range.py
@@ -0,0 +1,12 @@
+
+a = range( 0 , 100 , 2)
+print(list(a))
+
+b = range( 0 , 100 , 3)
+print(list(b))
+
+x = range( 100 , 0 , 2)
+print(list(x))
+
+y = range( 0 , 100 , 5)
+print(list(y))
